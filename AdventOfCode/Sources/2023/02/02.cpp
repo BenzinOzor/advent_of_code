4f848456937171d881380532e1c6368b098fee99
Benzine <fryfonbly@gmail.com>
2023-12-09 18:42:04 +0100

    [02] part 02 completed

--- AdventOfCode/Sources/2023/02/02.cpp
+++ AdventOfCode/Sources/2023/02/02.cpp
@@ -18,7 +18,7 @@ namespace y_2023
 		struct PuzzleVariables
 		{
 			std::vector< Sets > m_games{};
-			Set					m_inventory{ 12, 13, 14 };
+			Set					m_inventory{};
 			uint32_t			m_sum{ 0 };
 		};
 
@@ -58,7 +58,19 @@ namespace y_2023
 			return !(_set.m_red > g_puzzle_variables.m_inventory.m_red || _set.m_green > g_puzzle_variables.m_inventory.m_green || _set.m_blue > g_puzzle_variables.m_inventory.m_blue );
 		}
 
-		void parse_game( const std::string& _line )
+		void adapt_inventory_to_set( const Set& _set )
+		{
+			if( _set.m_red > g_puzzle_variables.m_inventory.m_red )
+				g_puzzle_variables.m_inventory.m_red = _set.m_red;
+
+			if( _set.m_green > g_puzzle_variables.m_inventory.m_green )
+				g_puzzle_variables.m_inventory.m_green = _set.m_green;
+
+			if( _set.m_blue > g_puzzle_variables.m_inventory.m_blue )
+				g_puzzle_variables.m_inventory.m_blue = _set.m_blue;
+		}
+
+		void parse_game_part_01( const std::string& _line )
 		{
 			g_puzzle_variables.m_games.push_back( {} );
 			auto& sets_vector = g_puzzle_variables.m_games.back();
@@ -77,7 +89,7 @@ namespace y_2023
 			if( valid_game )
 			{
 				g_puzzle_variables.m_sum += static_cast< uint32_t >( g_puzzle_variables.m_games.size() );
-				LOG_C( LogColor::dark_green, "%s // sum: %d", _line.c_str(), g_puzzle_variables.m_sum );
+				LOG_C( LogColor::dark_green, "%s || sum: %d", _line.c_str(), g_puzzle_variables.m_sum );
 			}
 			else
 			{
@@ -85,11 +97,45 @@ namespace y_2023
 			}
 		}
 
+		void parse_game_part_02( const std::string& _line )
+		{
+			auto& inventory = g_puzzle_variables.m_inventory;
+			inventory = Set{};
+
+			g_puzzle_variables.m_games.push_back( {} );
+			auto& sets_vector = g_puzzle_variables.m_games.back();
+
+			auto game = _line.substr( _line.find_first_of( ':' ) + 1 );
+			auto sets = split( game, ';' );
+
+			for( auto set : sets )
+			{
+				sets_vector.push_back( get_cube_set( extract_numbers( set, ' ' ), extract_words( set, ' ' ) ) );
+
+				adapt_inventory_to_set( sets_vector.back() );
+			}
+
+			auto power = inventory.m_red * inventory.m_green * inventory.m_blue;
+
+			g_puzzle_variables.m_sum += power;
+
+			LOG( "%s || inv R:%u G:%u B:%u / pow:%u / sum: %d", _line.c_str(), inventory.m_red, inventory.m_green, inventory.m_blue, power, g_puzzle_variables.m_sum );
+		}
+
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		void part_01( uint32_t _year, uint32_t _day )
 		{
 			g_puzzle_variables = PuzzleVariables{};
-			parse_input( _year, _day, FileType::PuzzleInput, parse_game );
+			g_puzzle_variables.m_inventory = Set{ 12, 13, 14 };
+			parse_input( _year, _day, FileType::PuzzleInput, parse_game_part_01 );
+			LOG_PRIO( LogColor::green, "Total sum: %u", g_puzzle_variables.m_sum );
+		}
+
+		void part_02( uint32_t _year, uint32_t _day )
+		{
+			g_puzzle_variables = PuzzleVariables{};
+			parse_input( _year, _day, FileType::PuzzleInput, parse_game_part_02 );
+			LOG_PRIO( LogColor::green, "Total sum: %u", g_puzzle_variables.m_sum );
 		}
 	}
 
@@ -108,5 +154,6 @@ namespace y_2023
 		LOG_NEW_LINE();
 		LOG_PRIO( LogColor::yellow, "Part 2" );
 
+		d_02::part_02( year, day );
 	}
 }
